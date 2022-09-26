c01b61f20304e87cca8c4ce3cd6a1af5abd7b7c4
Sugarman <1216008788@qq.com>
2022-09-26 16:23:12 +0800

    jsx支持

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 export default defineConfig({
-    plugins: [vue()],
-    esbuild: {
-        jsxFactory: 'h',
-        jsxFragment: 'Fragment'
-    }
+    plugins: [vue(),vueJsx()]
 })
