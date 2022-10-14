d772b097bfcd153dce8d4131a2c8c83a35f4806a
Sugarman <1216008788@qq.com>
2022-10-14 15:11:17 +0800

    优化layout和sider

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,34 +3,47 @@ import {defineComponent,h} from 'vue'
 const prefix = 't-layout'
 export default defineComponent({
     name: 'Layout',
-    provide(){
-        let withHeader = false
-        let withSider = false
-        const slots = this.$slots.default && this.$slots.default()
-        for(const item of slots){
-            if(item.type.name === 'Header') withHeader = true
-            if(item.type.name === 'Sider') withSider = true
-        }
-        return {
-            withHeader,
-            withSider
-        }
-    },
-    inject:{
+    props:{
         withHeader:{
+            type:Boolean,
             default:false
         },
-        withSider:{
+        withLeftSider:{
+            type:Boolean,
+            default:false
+        },
+        withRightSider:{
+            type:Boolean,
             default:false
         }
     },
     render () {
+
+        let layout;
+        let withHeader = false
+        let withRightSider = false
+        let withLeftSider = false
         const slots = this.$slots.default && this.$slots.default()
+        for(const item of slots){
+            if(item.type.name === 'Layout' && !layout) layout = item
+            if(item.type.name === 'Header') withHeader = true
+            if(item.type.name === 'Sider'){
+                if(layout){
+                    withRightSider = true
+                    if(!item['props']) item['props'] = {}
+                    item['props']['rightSider'] = true
+                }else{
+                    withLeftSider = true   
+                }
+            }
+        }
+        if(layout) layout['props'] = { withHeader, withRightSider, withLeftSider }
         return (
             <div class={{
                 [prefix]:true,
                 [`${prefix}-with-header`]:this.withHeader,
-                [`${prefix}-with-sider`]:this.withSider,
+                [`${prefix}-with-left-sider`]:this.withLeftSider,
+                [`${prefix}-with-right-sider`]:this.withRightSider,
             }}>{slots}</div>
         )
     }
--- src/components/Layout/sider.tsx
+++ src/components/Layout/sider.tsx
@@ -4,15 +4,20 @@ const prefix = 't-layout-sider'
 export default defineComponent({
     name: 'Sider',
     props: {
-        class:String
+        class:String,
+        rightSider:{
+            type:Boolean,
+            default:false
+        }
     },
     render () {
         const slots = this.$slots.default && this.$slots.default()
         return (
-            <div class={[
-                prefix,
-                this.class
-            ]}>{slots}</div>
+            <div class={{
+                [prefix]:true,
+                [this.class]:this.class,
+                [`${prefix}-right`]:this.rightSider
+            }}>{slots}</div>
         )
     }
 })
