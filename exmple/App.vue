edcd6710bc33ab19a9e551a00b3f89f92e42ad51
Sugarman <1216008788@qq.com>
2022-10-17 11:19:27 +0800

    layout Submenu

--- exmple/App.vue
+++ exmple/App.vue
@@ -31,6 +31,16 @@
                             <Menu name="3-1">下拉3</Menu>
                         </MenuGroup>
                     </Submenu>
+                    <Submenu name="3">
+                        <template #title>
+                            <Icon icon="&#xe7b4;" />带下拉的菜单
+                        </template>
+                        <MenuGroup title="一号" vertical>
+                            <Menu name="3-1">下拉1</Menu>
+                            <Menu name="3-1">下拉2</Menu>
+                            <Menu name="3-1">下拉3</Menu>
+                        </MenuGroup>
+                    </Submenu>
                 </MenuGroup>
             </Sider>
             <Layout>
--- src/components/Menu/submenu.tsx
+++ src/components/Menu/submenu.tsx
@@ -1,28 +1,43 @@
-import {defineComponent,h} from 'vue'
+import {defineComponent,h,ref} from 'vue'
 
 const prefix = 't-submenu'
+import CollapseTransition from '../CollapseTransition'
+import Icon from '../icon'
 export default defineComponent({
     name: 'Submenu',
     props: {
         
     },
-    methods: {
-        handleClickLink (event) {
-            this.$emit('click', event);
+    data() {
+        return {
+            showSubmenu:true
         }
     },
+    inject:['vertical'],
     render () {
         const title = this.$slots.title && this.$slots.title()
         const slots = this.$slots.default && this.$slots.default()
+
+        const aniTrigger = ()=> {
+            this.showSubmenu = !this.showSubmenu
+        }
         return (
             <div class={[prefix]}>
                 {title?
-                    <div class={{
-                        [`${prefix}-title`]:true
-                    }}>{title}</div>
+                    <div onClick={aniTrigger} class={{
+                        [`${prefix}-title`]:true,
+                        [`${prefix}-title-active`]:this.showSubmenu,
+                    }}>
+                        <span>{title}</span>
+                        <Icon icon="&#xe846;" size={12} />
+                    </div>
                 :null}
                 
-                {slots}
+                {this.vertical?<CollapseTransition>
+                    {this.showSubmenu?<div>{slots}</div>:null}
+                </CollapseTransition>:null}
+                
+                
             </div>
         )
     }
